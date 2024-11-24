217f171a258674437a44c2cb119688e8b83b7b76
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-11-23 23:05:12 -0500

    Initial

---
+++ PCATSNEPlugin.R
@@ -0,0 +1,18 @@
+library(SINCERA)
+
+input <- function(inputfile) {
+   mySC <<- readRDS(inputfile)
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+
+# do tSNE using PCA components
+sc <- doTSNE(mySC, genes=NULL, dims = 1:5, use.fast = T)
+pdf(paste(outputfile, "pdf", sep="."))
+saveRDS(sc, paste(outputfile, "rds", sep="."))
+# plot cells in tSNE spaces
+plotRDS(sc, feature.type="tsne")
+
+}
